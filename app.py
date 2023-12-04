62d5dfabeeaf81570bfe02f7980d03e6f9c8f19d
JEDG <116592477+jedg75@users.noreply.github.com>
2023-12-04 11:26:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,6 @@ def get_stored_data():
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run("app:app", host='127.0.0.1', port=8000)
+uvicorn.run("app:app", host='127.0.0.1', port=8000)
 
 #uvicorn app:app --reload
